30bddb4d969fa20cb0aa547e7611460287b85564
TWhittington <TWhittington@woodmenlife.org>
2023-06-13 14:45:15 -0500

    created types for auth context and use effect hooks

---
+++ app/src/context/AuthProvider.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+
+interface AuthContextValues {
+  authInfo: AuthInfo;
+  isAuthenticated: boolean;
+  setAuthInfo: (authInfo: AuthInfo) => void;
+  isAdmin: boolean;
+
+}
+export const AuthContext = React.createContext<undefined | AuthContextValues>(undefined);
+const Provider = AuthContext.Provider;
+
+interface Props {
+  children: React.ReactNode;
+}
+interface UserData {
+  role: 'USER' | 'ADMIN';
+}
+
+interface AuthInfo {
+  userData: UserData | null;
+}
+
+
+export function AuthProvider({ children }: Props) {
+  const [authInfo, setAuthInfo] = React.useState<AuthInfo>({
+    userData: null,
+  });
+
+  function handleAuthInfo(authInfo: AuthInfo) {
+    setAuthInfo(authInfo);
+  }
+  const isAuthenticated = authInfo.userData !== null;
+
+  const isAdmin = authInfo.userData?.role === "ADMIN";
+
+  return (
+    <Provider value={{ authInfo, isAuthenticated, setAuthInfo: handleAuthInfo, isAdmin }}>
+      {children}
+    </Provider>
+  );
+}
+
+export function useAuthContext() {
+  const context = React.useContext(AuthContext);
+
+  if (context === undefined) {
+    // handle
+    throw new Error("useAuthContext should be used iwthin an AuthProvider.")
+  }
+
+  return context;
+ }
